e7ac39abdf1cba430a85c32f577a7d4ab9a25fff
riskaimhoet <rainiputri1202@gmail.com>
2026-07-25 09:57:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ document.getElementById("typedMessage");
 
 let opened = false;
 
+const giftScene =
+document.getElementById("giftScene");
+
+const giftBox =
+document.getElementById("giftBox");
+
+const questionBox =
+document.getElementById("questionBox");
+
+const giftItems =
+document.querySelectorAll(".gift-item");
+
+const background =
+document.querySelector(".background");
+
+
 function typeWriter(text){
 
     let index = 0;
@@ -26,11 +42,31 @@ function typeWriter(text){
 
             clearInterval(typing);
 
+            setTimeout(() => {
+
+                envelope.classList.add("fade-out");
+                document
+       			.querySelector(".letter-text")
+      			.classList.add("fade-out");
+
+                typedMessage.classList.add("fade-out");
+
+
+            }, 1000);
+
+
+            setTimeout(() => {
+
+                showGiftScene();
+
+
+            }, 2500);
         }
 
     }, 90);
 
 }
+
 
 seal.addEventListener("click", () => {
 
@@ -86,3 +122,61 @@ seal.addEventListener("click", () => {
     }, 1800); 
 
 });
+
+function showGiftScene(){
+
+    giftScene.style.display = "flex";
+
+}
+
+giftBox.addEventListener("click", () => {
+
+    giftBox.src = "openedgift.PNG";
+
+    giftItems.forEach(item => {
+
+        item.classList.add("explode");
+
+    });
+
+    background.classList.add("change");
+
+    setTimeout(() => {
+
+        questionBox.classList.add("show");
+
+    }, 7000);
+
+    setTimeout(() => {
+
+        giftBox.style.opacity = "0";
+
+    }, 3000);
+
+});
+
+/* ========================= */
+/* Go to Page2 */
+/* ========================= */
+
+const yesBtn =
+document.getElementById("yesBtn");
+
+const noBtn =
+document.getElementById("noBtn");
+
+function goToPage2(){
+
+    document.body.classList.add("fade-out");
+
+    setTimeout(()=>{
+
+        window.location.href="page2.html";
+
+    },800);
+
+}
+
+yesBtn.onclick=goToPage2;
+
+noBtn.onclick=goToPage2;
